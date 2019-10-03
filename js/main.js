f152912c40f03ba3c09f763e09efe5f50ec8109e
Kevin Anantha <kevin.anantha@gmail.com>
2019-10-04 02:42:55 +0700

    :sparkles: list upcoming

--- js/main.js
+++ js/main.js
@@ -1,6 +1,59 @@
 $(document).ready(function() {
-  afterAuth()
+  isAuth()
+  axios({
+    method: 'get',
+    url: 'http://localhost:3000/home'
+  })
+    .then(({ data }) => {
+      data.map(res => {
+        $('#content').append(`
+        <div class="col-md-6">
+          <div class="card mb-3" style="max-width: 540px;">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <img src="${res.image_url}" class="card-img" alt="${res.title}">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">
+                    <a href="${res.url}">${res.title}</a>
+                  </h5>
+                  <p class="card-text">${res.synopsis}</p>
+                  <p class="card-text"><small class="text-muted">Start Date: ${res.start_date ? res.start_date : 'N/A'}</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-xl">Extra large modal</button>
+
+        <div class="modal fade bd-example-modal-xl" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+              ...
+            </div>
+          </div>
+        </div>
+        `)
+      })
+    })
+    .catch(err => {
+      swal.showValidationMessage(err.message)
+    })
 })
+
+function isAuth() {
+  if (localStorage.getItem('token')) {
+    $('.login').hide()
+    $('.register').hide()
+    $('.content').show()
+  } else {
+    $('.login').show()
+    $('.register').hide()
+    $('.content').hide()
+  }
+}
 
 function onSignIn(googleUser) {
   const id_token = googleUser.getAuthResponse().id_token;
@@ -21,7 +74,7 @@ function onSignIn(googleUser) {
   })
     .then(({ data }) => {
       localStorage.setItem('token', data.token)
-      afterAuth()
+      isAuth()
       Swal.fire({
         type: 'success',
         title: 'Logged in successfully',
@@ -38,7 +91,7 @@ function signOut() {
   const auth2 = gapi.auth2.getAuthInstance()
   auth2.signOut().then(function() {
     localStorage.removeItem('token')
-    afterAuth()
+    isAuth()
     $('#email').val('')
     $('#password').val('')
     Swal.fire({
@@ -48,17 +101,6 @@ function signOut() {
       timer: 1500
     })
   })
-}
-
-function afterAuth() {
-  if (localStorage.getItem('token')) {
-    $('.login').hide()
-    $('.content').show()
-    $('#formRegister').hide()
-  } else {
-    $('.login').show()
-    $('.content').hide()
-  }
 }
 
 $('#formRegister').on('submit', function(e) {
@@ -79,7 +121,7 @@ $('#formRegister').on('submit', function(e) {
   })
     .then((res) => {
       localStorage.setItem('token', res.data.token)
-      afterAuth()
+      isAuth()
       swal.close()
       Swal.fire({
         type: 'success',
@@ -111,15 +153,20 @@ $('#formLogin').on('submit', function(e) {
   })
     .then((res) => {
       localStorage.setItem('token', res.data.token)
-      afterAuth()
+      isAuth()
       swal.close()
     })
     .catch(err => {
-      console.log(err)
+      swal.showValidationMessage(err.message)
     })
 })
 
-function signUpForm(){
+function signUpForm() {
   $('.login').hide()
-  $('.register').css('display', 'block')
+  $('.register').show()
+}
+
+function signInForm() {
+  $('.login').show()
+  $('.register').hide()
 }
