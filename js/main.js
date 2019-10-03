12ba961210e52daa5dbfe06890dca78b093558ee
Kevin Anantha <kevin.anantha@gmail.com>
2019-10-03 20:23:56 +0700

    :rocket: sign in

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
-  axios({
+  console.log('ready')
+  // axios({
     
-  })
+  // })
 })
+
+function onSignIn(googleUser) {
+  const id_token = googleUser.getAuthResponse().id_token;
+  axios({
+    method: 'post',
+    url: 'http://localhost:3000/users/gsignin',
+    data: {
+      token: id_token
+    }
+  })
+    .then((res) => {
+      console.log(res)
+    })
+    .catch(err => {
+      swal.showValidationMessage(err)
+    })
+}
+
+function signOut() {
+  const auth2 = gapi.auth2.getAuthInstance()
+  auth2.signOut().then(function() {
+    console.log('User signed out.')
+  })
+}
